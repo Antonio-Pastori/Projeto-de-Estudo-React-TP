2acbe7202b8aa9dddf9d453333a87ac2ff9e5cd0
Antônio Pastori <antonioberthespastori@gmail.com>
2025-01-26 15:46:20 -0300

    Organizada no Projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './Componentes/Banner/Banner.js';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bem-Vindo ao React!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
     </div>
   );
 }
